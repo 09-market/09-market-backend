45a25ebfe55c74fe83a324468b5820521359cb75
dev-yoonjung <dear__yoon@naver.com>
2022-04-02 01:00:03 +0900

    chore: Create a backend project

---
+++ gonggu-market-api/src/main/java/com/gonggu/market/api/GongguMarketApiApplication.java
@@ -0,0 +1,13 @@
+package com.gonggu.market.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongguMarketApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GongguMarketApiApplication.class, args);
+    }
+
+}
---
+++ gonggu-market-api/src/test/java/com/gonggu/market/api/GongguMarketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonggu.market.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongguMarketApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
