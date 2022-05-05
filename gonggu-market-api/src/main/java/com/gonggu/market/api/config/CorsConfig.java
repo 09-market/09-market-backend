094518ca5fa439fe77e1c9eae9ec892959196c68
seominah <seomiinah95@gmail.com>
2022-05-06 00:52:17 +0900

    cors 설정 수정

--- gonggu-market-api/src/main/java/com/gonggu/market/api/config/CorsConfig.java
+++ gonggu-market-api/src/main/java/com/gonggu/market/api/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true); // js 처리 허용
-        config.addAllowedOrigin("*"); // 모든 Ip에 응답 허용
+//        config.addAllowedOrigin("*"); // 모든 Ip에 응답 허용
         config.addAllowedHeader("*"); // 모든 header 에 응답 허용
         config.addAllowedMethod("*"); // post, get, put, delete, patch 요청 허용
         source.registerCorsConfiguration("/api/**", config);
--- gonggu-market-api/src/main/java/com/gonggu/market/api/config/WebConfig.java
+++ gonggu-market-api/src/main/java/com/gonggu/market/api/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.gonggu.market.api.config;
 
+import com.gonggu.market.api.config.jwt.JwtProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
