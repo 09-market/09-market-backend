7d61ec760778df356f70bb7d3f0e4e9390fdacd6
seominah <seomiinah95@gmail.com>
2022-05-12 22:15:21 +0900

    feat: 프로젝트 수정

--- gonggu-market-api/src/main/java/com/gonggu/market/api/domain/post/InstagramFeedRepository.java
+++
@@ -1,6 +0,0 @@
-package com.gonggu.market.api.domain.post;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InstagramFeedRepository extends JpaRepository<InstagramFeed, Long> {
-}
