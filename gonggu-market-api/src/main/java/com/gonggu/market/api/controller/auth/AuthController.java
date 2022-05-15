e061e6d0ca59e03094c056e7fe032f0fa89e8ca8
seominah <seomiinah95@gmail.com>
2022-05-15 22:59:55 +0900

    feat: 유저 프로필 수정

--- gonggu-market-api/src/main/java/com/gonggu/market/api/controller/auth/AuthController.java
+++ gonggu-market-api/src/main/java/com/gonggu/market/api/controller/auth/AuthController.java
@@ -1,11 +1,14 @@
 package com.gonggu.market.api.controller.auth;
 
 
+import com.gonggu.market.api.config.auth.PrincipalDetails;
 import com.gonggu.market.api.domain.user.User;
 import com.gonggu.market.api.dto.auth.SignupDto;
+import com.gonggu.market.api.dto.user.UserUpdateDto;
 import com.gonggu.market.api.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
@@ -25,5 +28,14 @@ public class AuthController {
     public ResponseEntity<SignupDto> signup(@RequestBody SignupDto dto) {
         SignupDto result = authService.signup(dto);
         return ResponseEntity.ok(result);
+    }
+
+    @PutMapping("/{userId}/update")
+    public ResponseEntity<?> update(@PathVariable Long userId,
+                                    @RequestBody UserUpdateDto dto,
+                                    @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        User userEntity = authService.update(userId, dto);
+        principalDetails.setUser(userEntity);
+        return ResponseEntity.noContent().build();
     }
 }
--- gonggu-market-api/src/main/java/com/gonggu/market/api/service/UserService.java
+++ gonggu-market-api/src/main/java/com/gonggu/market/api/service/UserService.java
@@ -34,30 +34,19 @@ public class UserService {
         this.passwordEncoder = passwordEncoder;
     }
 
-    public User update(Long id, UserUpdateDto dto) {
-        User userEntity = userRepository.findById(id).orElseThrow(() -> {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        });
-
-        String rawPassword = dto.getPassword();
-        String encPassword = passwordEncoder.encode(rawPassword);
-        userEntity.setPassword(encPassword);
-        userEntity.setNickname(dto.getNickname() == null ? userEntity.getNickname() : dto.getNickname());
-        userEntity.setMobile(dto.getMobile() == null ? userEntity.getMobile() : dto.getMobile());
-        userEntity.setDetailAddress(dto.getAddress() == null ? userEntity.getDetailAddress() : dto.getAddress());
-        if (dto.getZipcode() != null) {
-            Address address = addressRepository.findByZipcode(dto.getZipcode());
-            if (address == null) {
-                System.out.println("address : " + address.toString());
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-            }
-            userEntity.setAddress(address);
-        }
-        userEntity = userRepository.save(userEntity);
-        return userEntity;
+    public UserProfileDto profile(String userId) {
+        User userEntity = userRepository.findById(Long.valueOf(userId)).get();
+        return new UserProfileDto(
+                userEntity.getEmail(),
+                userEntity.getNickname(),
+                userEntity.getMobile(),
+                userEntity.getDetailAddress(),
+                userEntity.getAddress().getZipcode(),
+                userEntity.getPoint()
+        );
     }
 
-    public UserProfileDto profile(String userId, String token) {
+    public UserProfileDto detail(String userId, String token) {
         token = token.replace(JwtProperties.TOKEN_PREFIX, "");
         Long userIdFromToken = JWT.decode(token).getClaim("id").asLong();
         logger.info(userIdFromToken.toString());
