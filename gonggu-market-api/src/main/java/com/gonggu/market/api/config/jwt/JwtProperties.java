2f0c307df7803dca622908482ef9a9bf9a36c1a8
seominah <seominah95@gmail.com>
2022-06-04 01:09:31 +0900

    feat: cors 에러 수정

--- gonggu-market-api/src/main/java/com/gonggu/market/api/config/jwt/JwtProperties.java
+++ gonggu-market-api/src/main/java/com/gonggu/market/api/config/jwt/JwtProperties.java
@@ -2,7 +2,7 @@ package com.gonggu.market.api.config.jwt;
 
 public interface JwtProperties {
     String SECRET = "zeronine";
-    int EXPIRATION_TIME = 1000 * 60 * 30;
+    int EXPIRATION_TIME = 1000 * 60 * 60;
     String TOKEN_PREFIX = "Bearer ";
     String HEADER_STRING = "Authorization";
 }
