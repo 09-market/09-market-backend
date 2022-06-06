407c0cd3bdc5f792b3516fece7dc421b1c3f63b5
seominah <seominah95@gmail.com>
2022-06-07 00:42:19 +0900

    feat: forbidden 에러 수정

--- gonggu-market-api/src/main/java/com/gonggu/market/api/config/SecurityConfig.java
+++ gonggu-market-api/src/main/java/com/gonggu/market/api/config/SecurityConfig.java
@@ -36,10 +36,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
         http
                 .cors()
                 .and()
-                .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .formLogin().disable()
