e822bfe158d4fd63c203a3870bb7a12dc86d809d
seominah <seominah95@gmail.com>
2022-06-16 00:14:59 +0900

    feat: Item 응답의 댓글 필드 변경

--- gonggu-market-api/src/main/java/com/gonggu/market/api/controller/dto/item/ItemDetailDto.java
+++ gonggu-market-api/src/main/java/com/gonggu/market/api/controller/dto/item/ItemDetailDto.java
@@ -1,8 +1,10 @@
 package com.gonggu.market.api.controller.dto.item;
 
+import com.gonggu.market.api.controller.dto.comment.CommentInItemDto;
 import com.gonggu.market.api.domain.comment.Comment;
 import com.gonggu.market.api.domain.item.Item;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ItemDetailDto {
@@ -15,7 +17,7 @@ public class ItemDetailDto {
     private String category;
     private String instagramUrl;
     private int likes;
-    private List<Comment> comments;
+    private List<CommentInItemDto> comments = new ArrayList<>();
     private Long userId;
     private String userImageUrl;
     private String nickname;
@@ -33,13 +35,22 @@ public class ItemDetailDto {
         this.category = item.getCategory().getCategoryName();
         this.instagramUrl = item.getInstagramUrl();
         this.likes = item.getLikes();
-        this.comments = item.getComments();
+        item.getComments().forEach(comment -> {
+            comments.add(new CommentInItemDto(
+                    comment.getContent(),
+                    comment.getItem().getId(),
+                    comment.getUser().getNickname(),
+                    comment.getUser().getId(),
+                    comment.getUser().getUserImageUrl()
+            ));
+        });
+        this.comments = comments;
         this.userId = item.getUser().getId();
         this.userImageUrl = item.getUser().getUserImageUrl();
         this.nickname = item.getUser().getNickname();
     }
 
-    public ItemDetailDto(Long itemId, String itemImageUrl, String name, String itemInfo, Integer price, Integer amount, String category, String instagramUrl, int likes, List<Comment> comments, Long userId, String userImageUrl, String nickname) {
+    public ItemDetailDto(Long itemId, String itemImageUrl, String name, String itemInfo, Integer price, Integer amount, String category, String instagramUrl, int likes, List<CommentInItemDto> comments, Long userId, String userImageUrl, String nickname) {
         this.itemId = itemId;
         this.itemImageUrl = itemImageUrl;
         this.name = name;
@@ -127,11 +138,11 @@ public class ItemDetailDto {
         this.likes = likes;
     }
 
-    public List<Comment> getComments() {
+    public List<CommentInItemDto> getComments() {
         return comments;
     }
 
-    public void setComments(List<Comment> comments) {
+    public void setComments(List<CommentInItemDto> comments) {
         this.comments = comments;
     }
 
